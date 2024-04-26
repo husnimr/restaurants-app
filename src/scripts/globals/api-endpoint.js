c163e97b8b5a5789cb86b3e1605ab2ae2b22e0f5
husnimr <husniramadhan@apps.ipb.ac.id>
2024-04-26 22:09:50 +0700

    commit

--- src/scripts/globals/api-endpoint.js
+++
@@ -1,8 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  RESTO: `${CONFIG.BASE_URL}list`,
-  RESTO_DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
-};
-
-export default API_ENDPOINT;
